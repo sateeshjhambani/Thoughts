dd5442500e9ed8da9dab5e14a27db4ad1616ad92
Sateesh <sateesh.jh@gmail.com>
2023-03-23 21:57:14 +0400

    Added the DI Setup

---
+++ app/src/main/java/com/sateeshjh/thoughts/NoteApp.kt
@@ -0,0 +1,8 @@
+package com.sateeshjh.thoughts
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp: Application() {
+}
---
+++ app/src/main/java/com/sateeshjh/thoughts/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.sateeshjh.thoughts.di
+
+import android.app.Application
+import androidx.room.Room
+import com.sateeshjh.thoughts.feature_note.data.data_source.NoteDatabase
+import com.sateeshjh.thoughts.feature_note.data.repository.NoteRepositoryImpl
+import com.sateeshjh.thoughts.feature_note.domain.repository.NoteRepository
+import com.sateeshjh.thoughts.feature_note.domain.use_case.DeleteNoteUseCase
+import com.sateeshjh.thoughts.feature_note.domain.use_case.GetNotesUseCase
+import com.sateeshjh.thoughts.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(noteDatabase: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(noteDatabase.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotesUseCase = GetNotesUseCase(repository),
+            deleteNoteUseCases = DeleteNoteUseCase(repository)
+        )
+    }
+}
